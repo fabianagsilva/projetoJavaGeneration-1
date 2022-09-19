c69ec007ea1925292970611ef64ddd609147582d
fabianagsilva <bisilvag2@gmail.com>
2022-09-18 22:56:19 -0300

    Implementação Classe Alunos - 18.09.22

--- src/projetoJavaGeneration/Alunos.java
+++ src/projetoJavaGeneration/Alunos.java
@@ -3,22 +3,23 @@ package projetoJavaGeneration;
 import java.util.Scanner;
 
 public class Alunos extends Escola {
-	
-	Scanner leia = new Scanner (System.in);
-	
+
+	Scanner leia = new Scanner(System.in);
+
+	int totalFaltas;
 	float mediaFinal;
 	private int id;
 	private int serie;
 	private String turma;
-	
-	public Alunos (String nome, String genero, String endereco, String cpf, int idade, int telefone, int id, int serie,
+
+	public Alunos(String nome, String genero, String endereco, String cpf, int idade, int telefone, int id, int serie,
 			String turma) {
 		super(nome, genero, endereco, cpf, idade, telefone);
-		
+
 		this.id = id;
 		this.serie = serie;
 		this.turma = turma;
-		
+
 	}
 
 	public int getId() {
@@ -44,30 +45,44 @@ public class Alunos extends Escola {
 	public void setTurma(String turma) {
 		this.turma = turma;
 	}
-	
-	
-	//Apenas uma ideia para pedirmos um input do usuário, o qual definirá as médias dos alunos e mostraremos o uso do Scanner
+
 	public void leiaMedia() {
 		float n1, n2, n3, n4;
-		System.out.println("Qual a média do(a) "+getNome()+" no 1º Bimestre? ");
+		System.out.println("\n\nQual a média do(a) " + getNome() + " no 1º Bimestre? ");
 		n1 = leia.nextFloat();
-		System.out.println("Qual a média do(a) "+getNome()+" no 2º Bimestre? ");
+		System.out.println("Qual a média do(a) " + getNome() + " no 2º Bimestre? ");
 		n2 = leia.nextFloat();
-		System.out.println("Qual a média do(a) "+getNome()+" no 3º Bimestre? ");
+		System.out.println("Qual a média do(a) " + getNome() + " no 3º Bimestre? ");
 		n3 = leia.nextFloat();
-		System.out.println("Qual a média do(a) "+getNome()+" no 4º Bimestre? ");
+		System.out.println("Qual a média do(a) " + getNome() + " no 4º Bimestre? ");
 		n4 = leia.nextFloat();
-		mediaFinal = (n1+n2+n3+n4)/4;
-		System.out.println("Média Final: "+mediaFinal);
-		
+		mediaFinal = (n1 + n2 + n3 + n4) / 4;
+		System.out.println("\n===== Média Final: " + mediaFinal + " =====");
+
 	}
-	
+
+	public void leiaFaltas() {
+		int f1, f2, f3, f4;
+		System.out.println("Quantas faltas " + getNome() + " teve no 1º Bimestre? ");
+		f1 = leia.nextInt();
+		System.out.println("Quantas faltas " + getNome() + " teve no 2º Bimestre? ");
+		f2 = leia.nextInt();
+		System.out.println("Quantas faltas " + getNome() + " teve no 3º Bimestre? ");
+		f3 = leia.nextInt();
+		System.out.println("Quantas faltas " + getNome() + " teve no 4º Bimestre? ");
+		f4 = leia.nextInt();
+		totalFaltas = f1 + f2 + f3 + f4;
+		System.out.println("\n===== Total de faltas: " + totalFaltas + " =====");
+
+	}
+
 	public void imprimirAluno() {
 		System.out.println("\n****************Aluno****************");
-		System.out.println("\nNome: "+getNome()+"\nGênero: "+getGenero()+"\nEndereço: "+getEndereco()+"\nCPF: "+getCpf()+"\nIdade: "
-				+getIdade()+"\nTelefone: "+getTelefone()+"\nRA: "+id+"\n"+serie+" série "+turma);
-		System.out.println("Média Final: "+mediaFinal);
+		System.out.println("\nNome: " + getNome() + "\nGênero: " + getGenero() + "\nEndereço: " + getEndereco()
+				+ "\nCPF: " + getCpf() + "\nIdade: " + getIdade() + "\nTelefone: " + getTelefone() + "\n\nRA: " + id
+				+ "\n" + serie + " série " + turma);
+		System.out.println("Média Final: " + mediaFinal);
+		System.out.println("Total de faltas: " + totalFaltas);
 
 	}
-
 }
